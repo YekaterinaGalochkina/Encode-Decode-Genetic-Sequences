3b204d65416724ef6203c019296fc55c46c882dc
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-03-15 00:01:48 -0700

    Fixing bugs

--- part_one.py
+++ part_one.py
@@ -11,9 +11,9 @@ def driver():
     ]
 
     categorized_sequences = {}
-    categorized_sequences["undetermined"] = [] # strands that can't be determined
-    categorized_sequences["dna"] = [] # dna strands
-    categorized_sequences["rna"] = [] # rna strands
+    categorized_sequences[-1] = [] # strands that can't be determined
+    categorized_sequences[0] = [] # dna strands
+    categorized_sequences[1] = [] # rna strands
 
     for sequence in all_sequences:
         category = categorize_strand(sequence)
@@ -65,15 +65,16 @@ def encode_strand(strand):
 
     encoding = []
     count = 1
+    strand = strand + ' '
 
     for index in range(1, len(strand)):
         if strand[index - 1] == strand[index]:
             count += 1
         else:
-            new_entry = strand[index - 1] + count
+            new_entry = strand[index - 1] + str(count)
             encoding.append(new_entry)
             count = 1
-
+    
     return "".join(encoding)
 
 def decode_strand(encoding):
